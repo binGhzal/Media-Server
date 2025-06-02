b5a9def6f9267a032f1775c375e86b2064d2e905
binghzal <suhail@binghzal.com>
2025-06-02 14:45:39 +0400

    Update .gitignore to include Terraform state and variable files; remove Ansible config files. Delete unused Kubernetes Terraform resources and add Proxmox VM configuration with necessary variables.

---
+++ terraform/proxmox/main.tf
@@ -0,0 +1,74 @@
+resource "proxmox_vm_qemu" "kube-node" {
+
+  # General Settings
+  count = var.vm_count
+  name  = "kube-node-${count.index + 1}"
+  desc  = "Ubuntu Server Kube Node"
+  agent = 1
+
+
+  target_node = var.node_name
+  vmid        = 2000 + count.index
+  tags        = "kube"
+
+  # Template Settings
+  clone_id   = var.template_id
+  full_clone = true
+
+  # Boot Processing Settings
+  onboot           = true
+  startup          = ""
+  automatic_reboot = true
+
+  # Hardware Settings
+  qemu_os = "l26"
+  bios    = "seabios"
+
+  # cpu Settings
+  cpu {
+    type    = "host"
+    cores   = var.cpu_cores
+    sockets = var.cpu_sockets
+  }
+
+  # Memory Settings
+  memory  = var.memory_size
+  balloon = var.balloon_size
+
+  # Network Settings
+  network {
+    id     = 0
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  # Disk Settings
+  scsihw = "virtio-scsi-single"
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = var.storage_pool
+        }
+      }
+    }
+    virtio {
+      virtio0 {
+        disk {
+          storage   = var.storage_pool
+          size      = var.disk_size
+          iothread  = true
+          replicate = false
+        }
+      }
+    }
+  }
+
+  # Cloud Init Settings
+  ipconfig0  = "ip=dhcp,ip6=dhcp"
+  nameserver = ""
+  ciuser     = var.cloud_init_user
+  cipassword = var.cloud_init_password
+  ciupgrade  = true
+  sshkeys    = var.public_ssh_key
+}
---
+++ terraform/proxmox/variables.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    proxmox = {
+      # LINK https://github.com/Telmate/terraform-provider-proxmox
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc9"
+    }
+  }
+}
+
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "node_name" {
+  type    = string
+  default = "your-proxmox-node"
+}
+
+variable "vm_count" {
+  type    = number
+  default = 1
+}
+
+variable "template_id" {
+  type    = number
+  default = 2000
+}
+
+variable "cpu_cores" {
+  type    = number
+  default = 1
+}
+
+variable "cpu_sockets" {
+  type    = number
+  default = 1
+}
+
+variable "memory_size" {
+  type    = number
+  default = 2048
+}
+
+variable "balloon_size" {
+  type    = number
+  default = 2048
+}
+
+variable "cloud_init_password" {
+  type      = string
+  sensitive = true
+}
+variable "public_ssh_key" {
+  type      = string
+  sensitive = true
+}
+variable "cloud_init_user" {
+  type    = string
+  default = "ubuntu"
+}
+
+variable "storage_pool" {
+  type    = string
+  default = "local-lvm"
+}
+
+variable "disk_size" {
+  type    = string
+  default = "20G"
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  # NOTE Optional, but recommended to set to true if you are using self-signed certificates.
+  pm_tls_insecure = true
+}
